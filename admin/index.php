994db31f219106c317ee1915e2061cdc3880f13f
Pratik Rauniyar <135122569+pratikrauniyar137@users.noreply.github.com>
2023-06-20 06:57:17 +0545

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<?php
+    require_once("inc/header.php");
+    require_once("inc/navigation.php");
+
+    if (isset($_GET['homepage']))
+    {
+        require_once("inc/homepage.php");
+    }else if(isset($_GET['addElectionPage']))
+    {
+        require_once("inc/add_election.php");
+    }else if (isset($_GET['addCandidatePage']))
+    {
+        require_once("inc/add_candidates.php");
+    }
+
+?>
+
+
+<?php
+    require_once("inc/footer.php");
+
+?>
---
+++ voters/inc/header.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+    require_once("../admin/inc/config.php");
+
+    if($_SESSION['key'] != "Voterskey")
+    {
+        echo "<script> location.assign('../admin/inc/logout.php'); </script>";
+        die;
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voters Panel - Online Candidate Registration </title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+    
+    <div class="container-fluid">
+        <div class="row bg-black text-white">
+            <div class="col-1">
+
+            <img src="../assets/images/logo.png" width="80px" />
+
+
+
+            </div>
+            <div class="col-11">
+                <h3> ONLINE CANDIDATE SYSTEM - <small> Welcome <?php echo $_SESSION['username']; ?></small> </h3>
+            </div>
+        </div>
+    </div>
